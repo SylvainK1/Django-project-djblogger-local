69f10498346e1f2670c060dfecfc92708f171d6d
SylvainK1 <sylvainkirouac@proton.me>
2024-02-24 13:54:51 -0500

    nc

--- djblogger/manage.py
+++ djblogger/manage.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+
+
 import sys
+
+
 from djblogger.settings import base
+
 
 def main():
     """Run administrative tasks."""
     if base.DEBUG:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djblogger.settings.local')
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djblogger.settings.local")
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djblogger.settings.production')
-    
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djblogger.settings.production")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -22,5 +27,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
