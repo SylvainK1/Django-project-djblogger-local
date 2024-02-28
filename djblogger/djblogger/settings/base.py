a936f70920cbda252a7b739e5909ae157619c3d8
SylvainK1 <sylvainkirouac@proton.me>
2024-02-27 20:02:44 -0500

    new changes
    just finish bootstrap installation

--- djblogger/djblogger/settings/base.py
+++ djblogger/djblogger/settings/base.py
@@ -7,7 +7,7 @@ import os
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -48,7 +48,7 @@ ROOT_URLCONF = "djblogger.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -115,3 +115,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
